c12852033f3ab1273718df02245586ef00c8933f
saidi <nourelislam.saidi@id-apps.fr>
2018-02-20 18:08:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/saidi/popularmoviesstageone/MoviesHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.saidi.popularmoviesstageone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_home);
+    }
+}
